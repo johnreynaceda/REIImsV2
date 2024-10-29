9f24849a89caf30d8a0b1c7e119b0be146774758
Johnrey Naceda <75570397+johnreynaceda@users.noreply.github.com>
2024-10-29 15:23:37 +0800

    Update statement-account.blade.php

--- resources/views/livewire/admin/statement-account.blade.php
+++ resources/views/livewire/admin/statement-account.blade.php
@@ -223,10 +223,10 @@
                                     <td class="border text-gray-700 text-sm font-bold text-left border-gray-700 px-3 ">
                                         BOOKS</td>
 
-                                    {{-- <td
+                                    <td
                                         class="border text-gray-700 text-sm font-medium text-center border-gray-700 px-3 ">
-
                                         @php
+                                            // Get book transaction IDs for the student
                                             $books = \App\Models\StudentTransaction::where(
                                                 'student_payment_id',
                                                 $dues->id,
@@ -234,7 +234,8 @@
                                                 ->pluck('id')
                                                 ->toArray();
 
-                                            $total_books = \App\Models\PaymentTransaction::whereIn(
+                                            // Count the number of paid book transactions
+                                            $paid_books_count = \App\Models\PaymentTransaction::whereIn(
                                                 'student_transaction_id',
                                                 $books,
                                             )
@@ -244,18 +245,19 @@
                                                 })
                                                 ->count();
 
+                                            // Set total_books based on book fee status
                                             if ($dues->book_fee_updated == false) {
                                                 $total_books = 500;
                                             } else {
-                                                $total_books =
-                                                    $dues->total_book == 1000
-                                                        ? 0
-                                                        : (float) $dues->total_book / (5 - $total_books);
+                                                // Prevent division by zero
+                                                $divisor = 5 - $paid_books_count;
+                                                $total_books = $divisor > 0 ? (float) $dues->total_book / $divisor : 0;
                                             }
                                         @endphp
 
                                         &#8369;{{ number_format($total_books, 2) }}
                                     </td>
+
                                     <td
                                         class="border text-gray-700 text-sm font-medium text-center border-gray-700 px-3 ">
                                         @if ($dues->book_fee_updated == false)
@@ -263,7 +265,7 @@
                                         @else
                                             &#8369;{{ number_format($dues->total_book, 2) }}
                                         @endif
-                                    </td> --}}
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td class="border text-sm text-gray-700 font-bold text-left border-gray-700 px-3">
@@ -466,7 +468,8 @@
                                             </svg>
                                             <svg wire:loading wire:target="view({{ $item->id }})"
                                                 class="animate animate-spin" width="15" height="15"
-                                                viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                viewBox="0 0 15 15" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
                                                 <path
                                                     d="M1.84998 7.49998C1.84998 4.66458 4.05979 1.84998 7.49998 1.84998C10.2783 1.84998 11.6515 3.9064 12.2367 5H10.5C10.2239 5 10 5.22386 10 5.5C10 5.77614 10.2239 6 10.5 6H13.5C13.7761 6 14 5.77614 14 5.5V2.5C14 2.22386 13.7761 2 13.5 2C13.2239 2 13 2.22386 13 2.5V4.31318C12.2955 3.07126 10.6659 0.849976 7.49998 0.849976C3.43716 0.849976 0.849976 4.18537 0.849976 7.49998C0.849976 10.8146 3.43716 14.15 7.49998 14.15C9.44382 14.15 11.0622 13.3808 12.2145 12.2084C12.8315 11.5806 13.3133 10.839 13.6418 10.0407C13.7469 9.78536 13.6251 9.49315 13.3698 9.38806C13.1144 9.28296 12.8222 9.40478 12.7171 9.66014C12.4363 10.3425 12.0251 10.9745 11.5013 11.5074C10.5295 12.4963 9.16504 13.15 7.49998 13.15C4.05979 13.15 1.84998 10.3354 1.84998 7.49998Z"
                                                     fill="currentColor" fill-rule="evenodd" clip-rule="evenodd">
